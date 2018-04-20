1f82d269a4d01a8c6f13390b6a06c5276aa5e669
Darren Anderson <dranders.radial@gmail.com>
2018-04-20 12:42:51 -0700

    Include nation-defs, which is a directory of YAML files, each file defining a member nation. Note the member jurisdictions are defined in each member-nation file.

--- model/index.js
+++ model/index.js
@@ -5,15 +5,44 @@ const debug = require('debug')('debug:model');
 const path   = require('path');
 const fs   = require('fs-extra');
 const yaml = require('js-yaml');
-
+const klawSync = require('klaw-sync');
+// Error Classes Used In This Module
 const MissingArgumentError = require('../config/errors/MissingArgumentError');
-
-let fieldDefs;
-
+// Module-Level "Constants"
+const YAML_EXTENSION = '.yml';
+const CONFIG_PATH = path.resolve(__dirname, '..', 'config');
+let fieldDefs; // this must be read and parsed (below)
+let nationDefs; // this must be read and parsed (below)
+// Read field definitions from a single YAML file
 try {
-    fieldDefs = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '..', 'config', 'field-defs.yml'), 'utf8'));
+    fieldDefs = yaml.safeLoad(fs.readFileSync(path.join(CONFIG_PATH, 'field-defs.yml'), 'utf8'));
 } catch (e) {
     throw(e);
+}
+// Read nationDefs from a directory, each nation having its own YAML-file definition
+{
+    nationDefs = [];
+    let paths = [];
+    try {
+        paths = klawSync(path.join(CONFIG_PATH, 'nation-defs'), {
+            nodir: true,
+            filter: (item) => path.extname(item.path) === YAML_EXTENSION,
+        });
+    }
+    catch (err) {
+        throw (err);
+    }
+    paths = paths.map((pathObj) => pathObj.path);
+    paths.forEach((filePath) => {
+
+        const baseName = path.basename(filePath, YAML_EXTENSION);
+        try {
+            // push each nationDef, also assigning the id using the file baseName (e.g. 'brazil.yml' is id'd as 'brazil')
+            nationDefs.push(Object.assign(yaml.safeLoad(fs.readFileSync(filePath, 'utf8')), { id: baseName }));
+        } catch (e) {
+            throw e;
+        }
+    });
 }
 
 const getEntries = async (regionId, lang) => {
